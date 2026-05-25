d1f7d2fe8a0a089269d7bf4db74d80b68c5a364c
nnkgw <nnkgw@users.noreply.github.com>
2026-05-26 08:11:19 +0900

    Update shirt and dress T-pose geometry
    Apply vertex and geometry updates to T-pose assets: numerous vertex positions were adjusted in bin/00016/shirt_tpose.obj and accompanying coarse geometry files, and corresponding coarse/fine geometry and maps for bin/03543 (dress_tpose) were updated. Also updated the simplify_03543.bat script. These changes correct/refine mesh vertex data and associated coarse representations for the models.

--- bin/simplify_03543.bat
+++ bin/simplify_03543.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
 ice-simplifier.exe 03543\dress.obj       03543\dress_coarse.obj       450
-ice-simplifier.exe 03543\dress_tpose.obj 03543\dress_tpose_coarse.obj 450
+ice-simplifier.exe 03543\dress_tpose.obj 03543\dress_tpose_coarse.obj 1500
 pause
